cee24a62d9fdccb5b99e715b266715d0fd5b8124
Leo espinoza <leo.spinoza@yahoo.com>
2019-01-09 14:41:57 -0600

    added redux devtools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {createStore, combineReducers} from 'redux'
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
 import {Provider} from 'react-redux'
 
 
@@ -17,7 +17,20 @@ const rootReducer = combineReducers({
   listItems: lists
 })
 
-const store = createStore(rootReducer)
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching', action);
+      const result = next(action);
+      console.log('[Middleware] next state', store.getState());
+      return result;
+    }
+  }
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)))
 
 ReactDOM.render(
   <Provider store={store}>
